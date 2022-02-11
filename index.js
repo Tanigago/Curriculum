69f9e29a634f7a01153d8db28f8795973352dae1
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-11 13:31:39 +0100

    function pointenter ALL added

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-function imprimeHola(){
-    console.log("Hola");
+function imprimeHello(){
+    console.log("Hello");
 }
 function main() {
     console.log("Starting...");
     const title = document.querySelector("h1")
-        title.addEventListener("pointerenter", imprimeHola)
+        title.addEventListener("pointerenter", imprimeHello)
+        //Las líneas 6 y 7 solo sirven para el primer h1 del doc html
+    const elements = Array.from(document.body.querySelectorAll("*"));
+//    const elem = document.body.querySelectorAll("*")
+    for (let item of elements){
+        console.log(item)
+        item.addEventListener("pointerenter", ()=>{console.log("PoP!")})
+    }
 }
 window.addEventListener("load",main);
