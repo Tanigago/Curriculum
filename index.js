4e4643bda56c677d70764eba2c77db05fce0e1ba
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-11 14:45:59 +0100

    elements.map counter added

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 function imprimeHello(){
     console.log("Hello");
 }
-function main() {
-    console.log("Starting...");
-    const title = document.querySelector("h1")
-        title.addEventListener("pointerenter", imprimeHello)
+   // const title = document.querySelector("h1")
+     //   title.addEventListener("pointerenter", imprimeHello)
         //Las líneas 6 y 7 solo sirven para el primer h1 del doc html
-    const elements = Array.from(document.body.querySelectorAll("*"));
+//    const elements = Array.from(document.body.querySelectorAll("*"));
 //    const elem = document.body.querySelectorAll("*")
-    for (let item of elements){
-        console.log(item)
-        item.addEventListener("pointerenter", ()=>{console.log("PoP!")})
-    }
+//    for (let item of elements){
+ //       console.log(item)
+//        item.addEventListener("pointerenter", ()=>{console.log("PoP!")})
+function main () {
+    console.log("Starting...");
+
+    const elements = Array.from(document.body.querySelectorAll("*"));
+
+    const counters = elements.map(
+        (item) => {
+            const obj = {
+                element: item,
+                counter: 0,
+            }
+            item.addEventListener("pointerenter",()=>{
+                obj.counter++;
+                console.log(obj)
+            });
+            return obj
+        }
+    )
 }
-window.addEventListener("load",main);
+
+window.addEventListener("load", main);
