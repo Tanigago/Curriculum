c52c6590f510e5ecd451e0dadbbd89374b1207ed
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-11 12:36:02 +0100

    JavaScript index added

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+function imprimeHola(){
+    console.log("Hola");
+}
 function main() {
     console.log("Starting...");
+    const title = document.querySelector("h1")
+        title.addEventListener("pointerenter", imprimeHola)
 }
 window.addEventListener("load",main);
